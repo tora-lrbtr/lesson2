10200a5b3be722df0c695c884b8479fcce7b3fa6
tora <102689338+tora-lrbtr@users.noreply.github.com>
2022-04-11 04:23:56 +0600

    ch
    code hello

---
+++ code.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class code {
+    public static void main(String[] args) {
+        System.out.println("Please enter your name:");
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+        System.out.println("Hello " + name);
+    }
+
+}
